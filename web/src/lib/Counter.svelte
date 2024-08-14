56b93243043fbb401a858b29043611a69ed2e7d1
Piotr Płaczek <piotrpdev@gmail.com>
2024-08-14 22:30:24 +0100

    feat: add biome and sass

--- web/src/lib/Counter.svelte
+++ web/src/lib/Counter.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+let count = 0;
+const increment = () => {
+	count += 1;
+};
 </script>
 
 <button on:click={increment}>
-  count is {count}
+  <p>count</p> is {count}
 </button>
+
+<style lang="scss">
+	button {
+    color: purple;
+    font-family: 'Comic Sans MS', cursive;
+    font-size: 2em;
+    
+    p {
+      color: red;
+    }
+	}
+</style>
