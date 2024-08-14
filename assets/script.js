4e2a89be31eeee129b33d4e60aad0589fc6b5480
Piotr Płaczek <piotrpdev@gmail.com>
2024-08-14 19:09:07 +0100

    feat: nicer html

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,7 @@ const socket = new WebSocket('ws://localhost:3000/ws');
 
 socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
+    const code = document.createElement("code")
+    code.innerText = event.data;
+    document.querySelector("#messages")?.appendChild(code);
 });
