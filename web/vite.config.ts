af565312eb9fa27601cb2fe265b2f9017a027fe5
Piotr Płaczek <piotrpdev@gmail.com>
2024-08-16 17:15:07 +0100

    refactor: import aliases

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,15 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@assets": path.resolve(__dirname, "./src/assets"),
+			"@lib": path.resolve(__dirname, "./src/lib"),
+		},
+	},
 	plugins: [svelte()],
 });
