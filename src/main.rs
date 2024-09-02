89e73de67bb33c935618c7304a619ab615008dde
Piotr Płaczek <piotrpdev@gmail.com>
2024-09-03 00:05:24 +0100

    feat: better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
 use std::fs::OpenOptions;
+use std::net::{IpAddr, Ipv4Addr};
 use std::path::PathBuf;
 use std::{convert::Infallible, net::SocketAddr};
 use std::{sync::Arc, time::Duration};
+
+use anyhow::{Context, Result};
 
 use axum::response::Redirect;
 use axum_server::tls_rustls::RustlsConfig;
@@ -25,6 +28,24 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use sysinfo::{Disks, Networks, ProcessesToUpdate, System};
 
 const SYSTEM_REFRESH_PERIOD: Duration = Duration::from_secs(5);
+const GRACEFUL_SHUTDOWN_PERIOD: Duration = Duration::from_secs(10);
+const ALIVE_CONNECTIONS_CHECK_PERIOD: Duration = Duration::from_secs(1);
+const SSE_KEEP_ALIVE_PERIOD: Duration = Duration::from_secs(1);
+
+const DEFAULT_IP_ADDRESS: std::net::IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+const DEFAULT_PORT: u16 = 3000;
+
+const DEFAULT_TSL_CERT_FILE_NAME: &str = "cert.pem";
+const DEFAULT_TLS_KEY_FILE: &str = "key.pem";
+
+const SYSTEM_STREAM_ERROR_DATA: &str = "Error occurred while attempting to process system stream";
+
+const DEFAULT_KERNEL_VERSION: &str = "v6.1";
+const DEFAULT_CPU_BRAND: &str = "Cortex-A72";
+const DEFAULT_CPU_BRAND_SHORT: &str = "Cortex-A";
+const DEFAULT_CPU_FREQUENCY: u64 = 1_800;
+const DEFAULT_DISK_SPACE: u64 = 32_000_000_000;
+const DEFAULT_MODEL_NAME: &str = "Raspberry Pi 4 Model B Rev 1.4";
 
 struct AppState {
     system_tx: broadcast::Sender<Event>,
@@ -41,18 +62,23 @@ async fn send_system_messages(state: Arc<AppState>) {
 
         let uptime = System::uptime();
 
-        let mut system = state.system.lock().await;
-        system.refresh_processes(ProcessesToUpdate::All);
-        let process_count = system.processes().len();
+        let process_count = {
+            let mut system = state.system.lock().await;
+            system.refresh_processes(ProcessesToUpdate::All);
+            system.processes().len()
+        };
 
-        let mut networks = state.networks.lock().await;
-        networks.refresh();
         let mut total_rx = 0;
         let mut total_tx = 0;
-        for (_interface_name, data) in networks.iter() {
-            total_rx += data.total_received();
-            total_tx += data.total_transmitted();
-        }
+        {
+            let mut networks = state.networks.lock().await;
+            networks.refresh();
+
+            for (_interface_name, data) in networks.iter() {
+                total_rx += data.total_received();
+                total_tx += data.total_transmitted();
+            }
+        };
 
         let event = Event::default().data(format!(
             "{total_rx:?}, {total_tx:?}, {process_count:?}, {uptime:?}"
@@ -62,27 +88,28 @@ async fn send_system_messages(state: Arc<AppState>) {
 }
 
 #[tokio::main]
-async fn main() {
-    let exe_path = env::current_exe().expect("Could not get path of exe");
-    let port_arg = std::env::args().nth(1).unwrap_or("3000".to_string());
-    let port: u16 = port_arg.parse().unwrap_or(3000);
+async fn main() -> Result<()> {
+    let exe_path = env::current_exe().context("Failed to get exe path")?;
+    let port = env::args().nth(1).map_or(DEFAULT_PORT, |port_arg| {
+        port_arg.parse().unwrap_or(DEFAULT_PORT)
+    });
 
-    let mut log_path;
-
-    if cfg!(debug_assertions) {
-        log_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let log_path = if cfg!(debug_assertions) {
+        let mut log_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         log_path.push("debug.log");
+        log_path
     } else {
-        log_path = exe_path.clone();
+        let mut log_path = exe_path.clone();
         log_path.pop();
         log_path.push("debug.log");
-    }
+        log_path
+    };
 
     let log_file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(log_path)
-        .expect("Could not open ./debug.log");
+        .open(log_path.clone())
+        .with_context(|| format!("Failed to open {}", log_path.display()))?;
 
     tracing_subscriber::registry()
         .with(
@@ -102,23 +129,23 @@ async fn main() {
         )
         .init();
 
-    let mut certs_path;
-
-    if cfg!(debug_assertions) {
-        certs_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let certs_path = if cfg!(debug_assertions) {
+        let mut certs_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         certs_path.push("certs");
+        certs_path
     } else {
-        certs_path = exe_path.clone();
+        let mut certs_path = exe_path.clone();
         certs_path.pop();
         certs_path.push("certs");
-    }
+        certs_path
+    };
 
     tracing::info!("Loading TLS .pem files from {certs_path:?}");
     // configure certificate and private key used by https
     let config =
-        RustlsConfig::from_pem_file(certs_path.join("cert.pem"), certs_path.join("key.pem"))
+        RustlsConfig::from_pem_file(certs_path.join(DEFAULT_TSL_CERT_FILE_NAME), certs_path.join(DEFAULT_TLS_KEY_FILE))
             .await
-            .unwrap();
+            .with_context(|| format!("Failed to read/find TLS {DEFAULT_TSL_CERT_FILE_NAME} and/or {DEFAULT_TLS_KEY_FILE} file(s) in {certs_path:?}"))?;
 
     // Create a new broadcast channel
     let (tx, _rx) = broadcast::channel(100);
@@ -127,7 +154,9 @@ async fn main() {
     let state = Arc::new(AppState {
         system_tx: tx,
         system: Mutex::new(System::new_all()),
-        kernel_version: Mutex::new(System::kernel_version().unwrap_or("v6.1".to_owned())),
+        kernel_version: Mutex::new(
+            System::kernel_version().unwrap_or_else(|| DEFAULT_KERNEL_VERSION.to_string()),
+        ),
         disks: Mutex::new(Disks::new_with_refreshed_list()),
         networks: Mutex::new(Networks::new_with_refreshed_list()),
     });
@@ -153,32 +182,37 @@ async fn main() {
         )
         .with_state(state);
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let addr = SocketAddr::from((DEFAULT_IP_ADDRESS, port));
     tracing::info!("Starting HTTPS server at {addr}");
     axum_server::bind_rustls(addr, config)
         .handle(handle)
         .serve(app.into_make_service())
         .await
-        .unwrap();
+        .with_context(|| format!("Failed to start HTTPS server at {addr}"))?;
 
     tracing::info!("Server has been shut down.");
+
+    Ok(())
 }
 
 // https://github.com/tokio-rs/axum/blob/1ac617a1b540e8523347f5ee889d65cad9a45ec4/examples/tls-graceful-shutdown/src/main.rs
 // https://github.com/programatik29/axum-server/blob/d48b1a931909d156177bc87684910769e67be905/examples/graceful_shutdown.rs
 async fn graceful_shutdown(handle: axum_server::Handle) {
     let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
+        signal::ctrl_c().await.unwrap_or_else(|e| {
+            tracing::warn!(error = %e, "Failed to install Ctrl+C handler");
+        });
     };
 
     #[cfg(unix)]
     let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
+        let signal_result = signal::unix::signal(signal::unix::SignalKind::terminate());
+        match signal_result {
+            Ok(mut s) => {
+                s.recv().await;
+            }
+            Err(e) => tracing::warn!(error = %e, "Failed to install Unix SIGTERM signal handler"),
+        }
     };
 
     #[cfg(not(unix))]
@@ -192,13 +226,12 @@ async fn graceful_shutdown(handle: axum_server::Handle) {
     tracing::info!("Received termination signal, shutting down...");
     // Refuses new connections
     // 10 secs is how long docker will wait to force shutdown
-    handle.graceful_shutdown(Some(Duration::from_secs(10)));
+    handle.graceful_shutdown(Some(GRACEFUL_SHUTDOWN_PERIOD));
 
     // Print alive connection count every second.
     loop {
-        sleep(Duration::from_secs(1)).await;
-
-        tracing::debug!("alive connections: {}", handle.connection_count());
+        sleep(ALIVE_CONNECTIONS_CHECK_PERIOD).await;
+        tracing::debug!("Alive connections: {}", handle.connection_count());
     }
 }
 
@@ -208,43 +241,70 @@ async fn sse_handler(
     let system_rx = state.system_tx.subscribe();
 
     let system_stream = tokio_stream::wrappers::BroadcastStream::new(system_rx)
-        .map(|msg| msg.unwrap_or_else(|_| Event::default().data("error")))
+        .map(|msg| msg.unwrap_or_else(|_| Event::default().data(SYSTEM_STREAM_ERROR_DATA)))
         .map(Ok);
 
-    Sse::new(system_stream).keep_alive(
-        axum::response::sse::KeepAlive::new()
-            .interval(Duration::from_secs(1))
-            .text("keep-alive-text"),
-    )
+    Sse::new(system_stream)
+        .keep_alive(axum::response::sse::KeepAlive::new().interval(SSE_KEEP_ALIVE_PERIOD))
 }
 
 async fn index_handler(state: State<Arc<AppState>>) -> impl IntoResponse {
     // TODO: Get model name from `tail -n 1 /proc/cpuinfo | cut -d':' -f2 | cut -c2-`
-    let system = state.system.lock().await;
-    let cpu_brand = system.cpus()[0].brand();
-    let disks = state.disks.lock().await;
-    let networks = state.networks.lock().await;
+
+    let (cpu_brand, cpu_count, cpu_speed, total_memory, process_count) = {
+        let system = state.system.lock().await;
+        (
+            system
+                .cpus()
+                .first()
+                .map_or_else(|| DEFAULT_CPU_BRAND.to_string(), |c| c.brand().to_string()),
+            system.cpus().len(),
+            system
+                .cpus()
+                .first()
+                .map_or(DEFAULT_CPU_FREQUENCY, sysinfo::Cpu::frequency),
+            system.total_memory(),
+            system.processes().len(),
+        )
+    };
+
+    let primary_disk_size = {
+        let disks = state.disks.lock().await;
+        (disks
+            .first()
+            .map_or(DEFAULT_DISK_SPACE, sysinfo::Disk::total_space)
+            / 1_000_000_000
+            + 7)
+            & !7
+    };
 
     let mut total_rx = 0;
     let mut total_tx = 0;
-    for (_interface_name, data) in networks.iter() {
-        total_rx += data.total_received();
-        total_tx += data.total_transmitted();
+    {
+        let networks = state.networks.lock().await;
+        for (_interface_name, data) in networks.iter() {
+            total_rx += data.total_received();
+            total_tx += data.total_transmitted();
+        }
     }
 
     let template = IndexTemplate {
         kernel_version: state.kernel_version.lock().await.to_string(), // 6.6.31+rpt-rpi-v8
-        model_name: "Raspberry Pi 4 Model B Rev 1.4".to_owned(),
+        model_name: DEFAULT_MODEL_NAME.to_string(),
         cpu_brand: cpu_brand.to_string(), // Cortex-A72
-        cpu_brand_short: cpu_brand[0..cpu_brand.len() - 2].to_string().to_uppercase(), // CORTEX-A
-        cpu_count: system.cpus().len(),   // 4
-        cpu_speed: system.cpus()[0].frequency(), // 1800 MHz
-        extended_memory: (system.total_memory() - (1_048_576_000)) / 1_000, // 4 GB
-        primary_disk_size: (disks[0].total_space() / 1_000_000_000 + 7) & !7, // 32 GB
-        total_memory: system.total_memory(), // 4 GB
-        rounded_memory: (system.total_memory() / 1_000_000_000 + 3) & !3, // 4 GB
+        cpu_brand_short: cpu_brand
+            .get(0..cpu_brand.len() - 2)
+            .unwrap_or(DEFAULT_CPU_BRAND_SHORT)
+            .to_string()
+            .to_uppercase(), // CORTEX-A
+        cpu_count,                        // 4
+        cpu_speed,                        // 1800 MHz
+        extended_memory: (total_memory - (1_048_576_000)) / 1_000, // 4 GB
+        primary_disk_size,                // 32 GB
+        total_memory,                     // 4 GB
+        rounded_memory: (total_memory / 1_000_000_000 + 3) & !3, // 4 GB
         uptime: System::uptime().to_string(),
-        process_count: system.processes().len(),
+        process_count,
         rx: total_rx,
         tx: total_tx,
     };
